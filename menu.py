4be35fcedf4a2afea438e785c21def284c9aa1df
Danielium <Lapshin_danila73@mail.ru>
2024-02-02 20:08:52 +0400

    Доработал код

--- menu.py
+++ menu.py
@@ -4,13 +4,19 @@ from telebot import types
 
 bot = telebot.TeleBot(bot_key)
 
-
+in_family = False #TODO если in_family = False, то при начале общения с ботом появляется кнопка "Добавиться в семью" и "Создать семью", нужно реализовать проверку на 'состоит ли пользователь в семье'
+identificator = 'aaa' #TODO реализовать генерацию уникального идентификатора для каждой семьи
 @bot.message_handler(commands=['start'])
 def start_message(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("Список покупок✍️")
-    btn2 = types.KeyboardButton("Настройки⚙️")
-    markup.add(btn1, btn2)
+    if in_family:
+        btn1 = types.KeyboardButton("Список покупок✍️")
+        btn2 = types.KeyboardButton("Настройки⚙️")
+        markup.add(btn1, btn2)
+    else:
+        btn1 = types.KeyboardButton("Добавиться в семью")
+        btn2 = types.KeyboardButton("Создать семью")
+        markup.add(btn1, btn2)
     bot.send_message(message.from_user.id, "Привет! Я семейный бот покупок.", reply_markup=markup)
 
 
@@ -33,7 +39,7 @@ def func(message):
     elif message.text == "Настройки⚙️":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = types.KeyboardButton("Семья")
-        btn2 = types.KeyboardButton("Оповещения") #через оповещения можно реализовать напоминания о покупке
+        btn2 = types.KeyboardButton("Оповещения") #через оповещения можно реализовать напоминания о покупке, либо можно бросить это дело
         back = types.KeyboardButton('Главное меню')
         markup.add(btn1, btn2)
         markup.add(back)
@@ -62,12 +68,17 @@ def func(message):
         bot.send_message(message.chat.id, text="Ваш список покупок успешно очищен", reply_markup=markup)
     elif message.text == "Семья":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Добавить члена семьи")
-        btn2 = types.KeyboardButton("Создать семью")
         back = types.KeyboardButton("Главное меню")
-        markup.add(btn1, btn2)
         markup.add(back)
-        bot.send_message(message.chat.id, text="Создать семью или добавить пользователя?", reply_markup=markup)
+        bot.send_message(message.chat.id, text=f"Уникальный идентификатор семьи: {identificator}", reply_markup=markup)
+    elif message.text == "Добавиться в семью":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        bot.send_message(message.from_user.id, "Введите уникальный идентификатор семьи, в которую хотите добавиться", reply_markup=markup)
+    elif message.text == "Создать семью":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Главное меню")
+        markup.add(btn1)
+        bot.send_message(message.from_user.id, "Вы успешно создали семью!\nПосмотреть уникальный идентификатор семьи, чтобы добавить других участников, вы можете в настройках ",reply_markup=markup)
 
 
 bot.polling(none_stop=True, interval=0)
